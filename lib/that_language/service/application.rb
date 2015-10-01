d372fe2714fafb02ee1d9d07559f65a7007a1a9e
Patrick Helm <me@patrick-helm.de>
2015-10-01 19:00:30 +0200

    Return proper json objects
    * Plus include confidence as attribute with /detect

--- lib/that_language/service/application.rb
+++ lib/that_language/service/application.rb
@@ -4,36 +4,50 @@ require "that_language"
 module ThatLanguage
   module Service
     class Application < Sinatra::Application
-      get '/' do
-        ThatLanguage.language_code(params['text'])
-      end
-
-      post '/' do
-        ThatLanguage.language_code(params['text'])
-      end
-
-      # get '/detect' do
-      #   ThatLanguage.detect(params['text']).to_json
-      # end
-      #
-      # post '/detect' do
-      #   ThatLanguage.detect(params['text']).to_json
-      # end
-
-      get '/details' do
-        ThatLanguage.details(params['text']).to_json
-      end
-
-      post '/details' do
-        ThatLanguage.details(params['text']).to_json
-      end
-
       get '/language_code' do
-        ThatLanguage.language_code(params['text']).to_json
+        render_json language_code: language_code
       end
 
       post '/language_code' do
-        ThatLanguage.language_code(params['text']).to_json
+        render_json language_code: language_code
+      end
+
+      get '/detect' do
+        render_json(detect)
+      end
+
+      post '/detect' do
+        render_json(detect)
+      end
+
+      get '/details' do
+        render_json details: details
+      end
+
+      post '/details' do
+        render_json details: details
+      end
+
+    private
+
+      def render_json(hash)
+        hash.to_json
+      end
+
+      def language_code
+        ThatLanguage.language_code(text)
+      end
+
+      def detect
+        ThatLanguage.detect(text)
+      end
+
+      def details
+        ThatLanguage.details(text)
+      end
+
+      def text
+        params['text']
       end
     end
   end
--- spec/that_language/service/application_spec.rb
+++ spec/that_language/service/application_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'rack/test'
+require 'json'
 
 # TODO: Complete all the specs
 describe ThatLanguage::Service::Application do
@@ -9,8 +10,36 @@ describe ThatLanguage::Service::Application do
     @app ||= described_class
   end
 
-  it "respond to /" do
-    get '?text=Hallo'
-    expect(last_response).to be_ok
+  describe "GET /language_code?text=Hallo" do
+    subject { last_response }
+    before { get('/language_code?text=Hallo') }
+
+    it { is_expected.to be_ok }
+
+    describe "response as a json" do
+      subject { JSON.parse(last_response.body) }
+
+      it { is_expected.to be_a(Hash) }
+      it { is_expected.to include("language_code" => "de") }
+    end
+  end
+
+  describe "GET /detect?text=Hallo" do
+    subject { last_response }
+    before { get('/detect?text=Hallo') }
+
+    it { is_expected.to be_ok }
+
+    describe "response as a json" do
+      subject { JSON.parse(last_response.body) }
+
+      it { is_expected.to be_a(Hash) }
+      it { is_expected.to include("language_code" => "de") }
+      it { is_expected.to include("confidence" => 1.0) }
+      it { is_expected.to include("value" => 1.0) }
+      it { is_expected.to include("hit_ratio" => 1.0) }
+      it { is_expected.to include("hit_count" => 1.0) }
+      it { is_expected.to include("words_count" => 1.0) }
+    end
   end
 end
